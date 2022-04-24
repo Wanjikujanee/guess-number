0b6adcc00e218fe56e0a76b020dd45828ef7d721
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-04-24 20:12:31 +0300

    python

---
+++ main.py
@@ -0,0 +1,12 @@
+color = input("enter a color:")
+plural_noun = input("enter a PLural noun:")
+celebrity = input("enter a celebrity:")
+adjective= input("enter an adjective:")
+story="I Love AkiraChix"
+
+
+print("Roses are " + color)
+print(plural_noun + " are Blue")
+print("I love " + celebrity)
+print("Food is " + adjective)
+print(story)
